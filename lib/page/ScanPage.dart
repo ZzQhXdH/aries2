61e8669d0ac63b39ed4a0fe26b51be08a9bae699
zzqh <xdhwwdz2011@163.com>
2019-05-27 20:26:34 +0800

    first commit

---
+++ lib/page/ScanPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ScanPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ScanState();
+  }
+}
+
+class _ScanState extends State<ScanPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
